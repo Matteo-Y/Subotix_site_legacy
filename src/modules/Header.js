5c1a3dc4bf3ff73f0cc38f4b32292aec8952a9ef
Matteo Yuen <matteo.yuen@gmail.com>
2023-05-03 14:18:04 -0500

    landing page template set.

---
+++ src/modules/Header.js
@@ -0,0 +1,11 @@
+
+export default function Header() {
+    return (
+        <>
+            <div className="head">
+                <h2 className="header-element">home</h2>
+                <h1>SUBOTIX</h1>
+            </div>
+        </>
+    );
+}
---
+++ src/modules/Landing.js
@@ -0,0 +1,16 @@
+
+export default function Landing() {
+    return (
+        <div className="landing-page">
+            <div className="hero section">
+                <p>[insert hero content]</p>
+            </div>
+            <div className="store section">
+                <p>[insert store content]</p>
+            </div>
+            <div className="contacts section">
+                <p>[insert contacts content]</p>
+            </div>
+        </div>
+    );
+}
