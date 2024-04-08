76bb859ba080dba9d5c1325830b3dcd5ac88f56a
Matteo Yuen <matteo.yuen@gmail.com>
2024-04-08 16:11:18 -0500

    Landing redo

--- src/modules/Landing.js
+++ src/modules/Landing.js
@@ -5,43 +5,9 @@ export default function Landing() {
     return (
         <div className="landing-page">
             <div className="hero section">
-                <img style={{marginRight: "5%"}} className="banner" src={require("../res/pictures/misc/Banner_0.jpg")} alt="home banner"  loading="lazy"/>
-                <div className="landing-textbox">
-                    <h2>about us</h2>
-                    <p>
-                       we create and manufacture innovative stem<br/>
-                       products to provide a next generation education
-                    </p>
-                </div>
+                <img className="banner" src={require("../res/pictures/misc/Banner_0.jpg")} alt="home banner"  loading="lazy"/>
             </div>
-            <a className="redirect-section reverse section" href={"#" + GALLERY_PATH}>
-                <div className="landing-textbox">
-                    <h2>gallery</h2>
-                    <p>
-                        a collection of projects<br/>
-                        and activities subotix has<br/> 
-                        been a part of.
-                    </p>
-                </div>
-                <div id="gallery-slideshow">
-                    <Slideshow delay={5000}>
-                        <img className="slide-image" src={require("../res/pictures/misc/printers_0.jpg")} alt="sawy banner"  loading="lazy"/>
-                        <img className="slide-image" src={require("../res/pictures/misc/robot_hand_0.jpg")} alt="sawy banner"  loading="lazy"/>
-                        <img className="slide-image" src={require("../res/pictures/misc/controller_0.jpeg")} alt="sawy banner"  loading="lazy"/>
-                    </Slideshow>
-                </div>
-            </a>
-            <a className="redirect-section section" href={"#" + SAWY_PATH}>
-                <div className="landing-textbox">
-                    <h2>sawy sawy</h2>
-                    <p>
-                        the perfect tool for beginners.<br/>
-                        a good lesson in conductivity, <br/>
-                        and basic circuitry.
-                    </p>
-                </div>
-                <video className="banner" src={require("../res/videos/sawy-sawy_commercial.mp4")} type="video/mp4" autoPlay muted loop/>
-            </a>
+            
         </div>
     );
 }
