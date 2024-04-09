979e485061ac4cbe5cfa3f6e5755d0eae50c1b1f
Matteo Yuen <matteo.yuen@gmail.com>
2024-04-08 21:07:13 -0500

    content update

--- src/modules/Landing.js
+++ src/modules/Landing.js
@@ -6,7 +6,7 @@ export default function Landing() {
     return (
         <div className="landing-page">
             <div id="hero-section" className="section">
-                <img className="banner" src={require("../res/pictures/gallery/4.jpg")} alt="home banner"  loading="lazy"/>
+                <img className="banner" src={require("../res/pictures/gallery/48.jpg")} alt="home banner"  loading="lazy"/>
                 <div id="hero-infobar">
                     <h2>subotix</h2>
                     <h3 style={{ width: "80%" }}>providing a next-gen education</h3>
@@ -16,11 +16,10 @@ export default function Landing() {
             <div id="product-section" className="section">
                 <h2 style={{marginLeft: "10%"}} className="section-title">products</h2>
                 <div id="product-card-display">
-                    <ProductCard productName="subysaurus" img={require("../res/pictures/gallery/5.jpg")}/>
-                    <ProductCard productName="subysaurus" img={require("../res/pictures/gallery/5.jpg")}/>
-                    <ProductCard productName="subysaurus" img={require("../res/pictures/gallery/5.jpg")}/>
-                    <ProductCard productName="subysaurus" img={require("../res/pictures/gallery/5.jpg")}/>
-                    <ProductCard productName="subysaurus" img={require("../res/pictures/gallery/5.jpg")}/>
+                    <ProductCard productName="sawysawy"        img={require("../res/pictures/gallery/63.jpg")}/>
+                    <ProductCard productName="subysaurus"      img={require("../res/pictures/gallery/5.jpg")}/>
+                    <ProductCard productName="robot hand kit"  img={require("../res/pictures/gallery/64.jpg")}/>
+                    <ProductCard productName="starship models" img={require("../res/pictures/gallery/65.jpg")}/>
                 </div>
             </div>
             <a id="sawy-section" className="section" href={"#" + SAWY_PATH}>
