31ce0a54d1866931bafdd5a25a2d95c2146477e5
Matteo Yuen <matteo.yuen@gmail.com>
2024-04-08 20:14:11 -0500

    sawy section redo

--- src/modules/InDevelopment.js
+++ src/modules/InDevelopment.js
@@ -1,8 +1,11 @@
+import { BASE_PATH } from "../App";
+
 export default function InDevelopment() {
 
     return <>
-        <div style={{padding: "8rem 0 8rem 0", marginTop: "50px"}} className="section">
-            <h3>page is currently being developed</h3>
+        <div style={{padding: "8rem 0 8rem 0", marginTop: "50px", flexDirection: "column"}} className="section">
+            <h3 style={{color: "black", textAlign: "center"}}>page is currently being developed</h3>
+            <a style={{color: "black", textDecoration: "none", fontSize: "3rem", textAlign: "center"}} href={BASE_PATH}>home</a>
         </div>
     </>
 }
--- src/modules/Landing.js
+++ src/modules/Landing.js
@@ -1,14 +1,15 @@
 import { SAWY_PATH, GALLERY_PATH } from "../App";
+import Collage from "./Collage";
 import ProductCard from "./ProductCard";
 
 export default function Landing() {
     return (
         <div className="landing-page">
             <div id="hero-section" className="section">
-                <img className="banner" src={require("../res/pictures/gallery/61.jpg")} alt="home banner"  loading="lazy"/>
+                <img className="banner" src={require("../res/pictures/gallery/4.jpg")} alt="home banner"  loading="lazy"/>
                 <div id="hero-infobar">
                     <h2>subotix</h2>
-                    <h3>providing next generation educational tools</h3>
+                    <h3 style={{ width: "80%" }}>providing a next-gen education</h3>
                     <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
                 </div>
             </div>
@@ -19,11 +20,27 @@ export default function Landing() {
                     <ProductCard productName="subysaurus" img={require("../res/pictures/gallery/5.jpg")}/>
                     <ProductCard productName="subysaurus" img={require("../res/pictures/gallery/5.jpg")}/>
                     <ProductCard productName="subysaurus" img={require("../res/pictures/gallery/5.jpg")}/>
+                    <ProductCard productName="subysaurus" img={require("../res/pictures/gallery/5.jpg")}/>
                 </div>
             </div>
-            <div id="sawy-section" className="section">
-                [insert sawy content]
-            </div>
+            <a id="sawy-section" className="section" href={"#" + SAWY_PATH}>
+                <div style={{display: "flex", flexDirection: "row", width: "100%"}}>
+                    <div style={{display: "flex", flexDirection: "collumn", width: "33%"}}>
+                        <img className="sawy-image" src={require("../res/pictures/gallery/3.jpg")} alt=""/>
+                    </div>
+                    <div style={{display: "flex", flexDirection: "collumn", width: "33%"}}>
+                        <img className="sawy-image" src={require("../res/pictures/gallery/40.jpg")} alt=""/>
+                    </div>
+                    <div style={{display: "flex", flexDirection: "collumn", width: "33%"}}>
+                        <img className="sawy-image" src={require("../res/pictures/gallery/5.jpg")} alt=""/>
+                    </div>
+                </div>
+                <div id="sawy-infobar">
+                    <h2>sawy-sawy</h2>
+                    <h3>out of the box ready microcontroller</h3>
+                    <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+                </div>
+            </a>
         </div>
     );
 }
