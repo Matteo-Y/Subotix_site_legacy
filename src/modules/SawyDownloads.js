3ccee3368fb5b2fcd35a817c3171548c5ba9b7e0
Matteo <matteo.yuen@gmail.com>
2024-04-22 10:19:03 -0500

    download page cleanup

--- src/modules/SawyDownloads.js
+++ src/modules/SawyDownloads.js
@@ -1,9 +1,13 @@
+import { SAWY_PATH } from "../App";
+
 export default function SawyDownloads() {
     return <div id="download-section">
+        <a className="back-to-sawy" href={"#" + SAWY_PATH}>{"\u2039"}back</a>
         <div id="download-table">
-            <div className="download-tab"><a className="download-link" href={require("../res/files/default_firmware.zip")}    download>default_factory_firmware</a></div>
-            <div className="download-tab"><a className="download-link" href={require("../res/files/finger_robot.zip")}        download>sawy-sawy_claw_robot</a></div>
-            <div className="download-tab"><a className="download-link" href={require("../res/files/line_follower_robot.zip")} download>line_follower_code</a></div>
+            <div style={{backgroundColor: "#333"}} className="download-tab"><h5 style={{margin: "5px", color: "white"}}>file downloads</h5></div>
+            <div style={{backgroundColor: "#bbb"}} className="download-tab"><a className="download-link" href={require("../res/files/default_firmware.zip")}    download>default_factory_firmware</a></div>
+            <div style={{backgroundColor: "#f9f9f9"}} className="download-tab"><a className="download-link" href={require("../res/files/finger_robot.zip")}        download>sawy-sawy_claw_robot</a></div>
+            <div style={{backgroundColor: "#bbb"}} className="download-tab"><a className="download-link" href={require("../res/files/line_follower_robot.zip")} download>line_follower_code</a></div>
         </div>
     </div>
 }
