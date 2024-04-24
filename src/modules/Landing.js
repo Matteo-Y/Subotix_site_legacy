dd6814d939c567b2ccbafc2268613c7252454b8d
Matteo <matteo.yuen@gmail.com>
2024-04-24 08:37:37 -0500

    homepage store links

--- src/modules/Landing.js
+++ src/modules/Landing.js
@@ -1,6 +1,5 @@
 import { BASE_PATH, SAWY_PATH, STORE_PATH } from "../App";
 import ProductCard from "./ProductCard";
-import { HashLink } from "react-router-hash-link";
 import Slideshow from "./slide/Slideshow";
 
 const imageFolder = require.context("../res/pictures/gallery/slideshow_1", true);
@@ -63,10 +62,10 @@ export default function Landing() {
             <div id="product-section" className="section">
                 <h2 style={{marginLeft: "10%"}} className="section-title">products</h2>
                 <div id="product-card-display">
-                    <ProductCard productName="sawysawy"        img={require("../res/pictures/gallery/63.jpg")}/>
-                    <ProductCard productName="subysaurus"      img={require("../res/pictures/gallery/5.jpg")}/>
-                    <ProductCard productName="robot hand kit"  img={require("../res/pictures/gallery/64.jpg")}/>
-                    <ProductCard productName="starship models" img={require("../res/pictures/gallery/65.jpg")}/>
+                    <ProductCard productName="sawysawy"        link={"#" + SAWY_PATH} img={require("../res/pictures/gallery/63.jpg")}/>
+                    <ProductCard productName="subysaurus"      link={"#" + STORE_PATH} img={require("../res/pictures/gallery/5.jpg")}/>
+                    <ProductCard productName="robot hand kit"  link={"#" + STORE_PATH} img={require("../res/pictures/gallery/64.jpg")}/>
+                    <ProductCard productName="starship models" link={"#" + STORE_PATH} img={require("../res/pictures/gallery/65.jpg")}/>
                 </div>
             </div>
             <a id="locations-section" className="section hover-section" href={"#" + BASE_PATH}>
@@ -76,7 +75,6 @@ export default function Landing() {
                         <h2>locations</h2>
                         <h3>visit us</h3>
                     </div>
-                    
                 </div>
             </a>
         </div>
--- src/modules/ProductCard.js
+++ src/modules/ProductCard.js
@@ -1,8 +1,8 @@
 export default function ProductCard(props) {
-    return <div className="product-card">
+    return <a className="product-card" href={props.link}>
         <img style={{position: "absolute", minHeight: "450px", width: "300px", objectFit: "cover"}} src={props.img} alt=""/>
         <h4 style={{position: "absolute", bottom: "0", backgroundColor: "rgba(0, 0, 0, 0.8)", color: "white", fontSize: "30px", fontWeight: "normal", width: "240px", height: "40px", margin: "0", padding: "30px", textAlign: "center"}}>
             {props.productName}
         </h4>
-    </div>
+    </a>
 }
