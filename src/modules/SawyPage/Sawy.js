2ee1cc3978835fca4f959f6898e7cfc4b1891cb9
Matteo Yuen <matteo.yuen@gmail.com>
2024-10-07 16:58:28 -0500

    sawy projects section

--- src/modules/SawyPage/Sawy.js
+++ src/modules/SawyPage/Sawy.js
@@ -10,13 +10,43 @@ export default function Sawy() {
                 <a id="schem-link" className="sawy-link-button" href={"#" + SAWY_PATH + "/downloads"}>schematics</a>
             </div>
         </div>
-        <div style={{height: "500px"}}>
-
+        <div id="sawy-projects">
+            <div id="sawy-projects-header">
+                <ProjectCard title="sawy-sawy model" image={require("../SawyPage/res/images/sawy.png")} imageAlt="sawy-thumbnail" reverse="true">
+                    sawy-sawy kits cater to different learners. <span style={{fontFamily: "roboto", fontWeight: "bolder"}}>HORIZONTAL</span> projects are accessible to everyone. <span style={{fontFamily: "roboto", fontWeight: "bolder"}}>VERTICAL</span> projects offer advanced challenges.
+                </ProjectCard>
+            </div>
+            <div id="sawy-projects-horizontals">
+                <ProjectCard title="horizontal kits" reverse="false">
+                    horizontal projects are user-friendly. they are perfect for beginners. with a focus on basic <span style={{fontFamily: "roboto", fontWeight: "bolder"}}>STEAM</span> principles.
+                </ProjectCard>
+                <ProjectCard title="sawy-sawy piano" image={require("../SawyPage/res/images/sawy-piano.png")} imageAlt="piano-thumbnail" reverse="false">
+                    the sawy-sawy piano kit is easy to use. it teaches basic concepts like conductivity, circuits and inputs.
+                </ProjectCard>
+                <ProjectCard title="the saqar" image={require("../SawyPage/res/images/dito.png")} imageAlt="dito-thumbnail" reverse="false">
+                    the sawy-sawy robot finger kit teaches essential skills in mechanics and actuators, equipping users for more advanced robotics projects.
+                </ProjectCard>
+            </div>
+            <div id="sawy-projects-verticals">
+                <ProjectCard title="vertical kits" reverse="true">
+                    vertical projects build on existing skills. they introduce complex concepts, encouraging <span style={{fontFamily: "roboto", fontWeight: "bolder"}}>CRITICAL THINKING</span> and <span style={{fontFamily: "roboto", fontWeight: "bolder"}}>INNOVATION</span>.
+                </ProjectCard>
+                <ProjectCard title="line-follower" reverse="true">
+                the line follower kit teaches you skills in robotics and automation. users will explore input/output concepts and optimize their robot’s performance.
+                </ProjectCard>
+                
+            </div>
         </div>
-        {/*<div id="sawy-disclaimer">
-            <h2>disclaimer</h2>
-            <p style={{fontSize: "1em", fontWeight: "lighter", padding: "0", margin: "0 50px 20px 50px"}}>the "sawy-sawy" is a visually modified "makey-makey" board produced by joylabz</p>
-            <a id="makeymakey-link" style={{fontWeight: "lighter", color: "white"}} href="https://makeymakey.com/pages/about-us" target="none" rel="noreferrer noopener">learn more</a>
-        </div>*/}
+        
     </div>;
+}
+
+function ProjectCard(props) {
+    return <div className="sawy-project-card">
+        <h3 className="sawy-project-card-title">{props.title}</h3>
+        <div style={props.reverse == "true" ? {display: "flex", flexDirection: "row-reverse"} : {display: "flex", flexDirection: "row"}}>
+            <img className="sawy-project-card-thumbnail" src={props.image} alt={props.imageAlt}/>
+            <p className="sawy-project-card-description">{props.children}</p>
+        </div>
+    </div>
 }
